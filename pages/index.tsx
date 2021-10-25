3c8340de3357d290f21b0d33fda933a39195174a
jefferson1104 <jefferson1104junior@gmail.com>
2021-10-25 18:26:16 -0300

    styling form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-    <h1>Contact Form</h1>
+    <div className="h-screen w-screen flex justify-center items-center bg-gray-200">
+      <form className="w-4/12 bg-white flex flex-col rounded-md	p-4 py-8 shadow-xl">
+        <input className="rounded border border-blue-200 p-1" placeholder="Your name" type="text" />
+        <input className="rounded border border-blue-200 p-1 mt-4" placeholder="Your email" type="text" />
+        <textarea className="rounded border border-blue-200 p-1 mt-4" placeholder="Write your message..." />
+        <button className="rounded bg-blue-500 text-white p-3 mt-4 font-bold hover:bg-blue-700 duration-500">
+          SEND MESSAGE
+        </button>
+      </form>
+    </div>
   )
 }
